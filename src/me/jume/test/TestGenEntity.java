13198865d85a8b945a72db29e7a995a72a2040c1
jume <jume@LAPTOP-S4T8477F>
2019-03-01 16:52:21 +0800

    3-1

--- src/me/jume/test/TestGenEntity.java
+++ src/me/jume/test/TestGenEntity.java
@@ -4,6 +4,7 @@ import me.jume.util.GenEntityMysqlUtil;
 
 public class TestGenEntity {
 	public static void main(String[] args) {
+		//demo
 		String packagePath = "me.jume.util.Account";
 		String tableName = "account";
 		GenEntityMysqlUtil.genEntity(packagePath, tableName);
