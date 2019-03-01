3294c278f2c81129a1121c7d74931badb7ef0e76
jume <jume@LAPTOP-S4T8477F>
2019-03-01 17:01:54 +0800

    commit to

--- src/me/jume/test/TestGenEntity.java
+++ src/me/jume/test/TestGenEntity.java
@@ -1,10 +1,10 @@
-package me.jume.test;
+ package me.jume.test;
 
 import me.jume.util.GenEntityMysqlUtil;
 
 public class TestGenEntity {
 	public static void main(String[] args) {
-		//demo
+		//this is test !
 		String packagePath = "me.jume.util.Account";
 		String tableName = "account";
 		GenEntityMysqlUtil.genEntity(packagePath, tableName);
